0846e722165260e3def07ca21db9b2bfcde3d9a2
parkjooyoung0608 <72453080+parkjooyoung0608@users.noreply.github.com>
2021-01-18 19:52:33 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -38,6 +38,32 @@ contactBtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 })
 
+
+// 스크롤이 내려가면 Home이 투명해짐
+// 1. 스크롤이 Home높이에서 내려갈수록 opacity 1 -> 0
+// 2. window.scrollY의 값이 home의 높이랑 같을 경우 opacity = 0
+//  : 1 - window.scrollY (400px) / homeHeight(800px) = 0.5 (opacity 값)
+//  : 1 - window.scrollY (800px) / homeHeight(800px) = 0 (opacity 값)
+const home = document.querySelector('.home__container')
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Function ScrollIntoView
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector)
